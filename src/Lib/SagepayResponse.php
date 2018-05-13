b4940fb8df958a7a4b089196320200199af0e12c
Santi Barbat <barbatsan@gmail.com>
2018-05-13 18:52:52 +0100

    Added sagepay response fields to response class

--- src/Lib/SagepayResponse.php
+++ src/Lib/SagepayResponse.php
@@ -26,6 +26,15 @@ class SagepayResponse
     protected $amount;
     protected $txAuthNo;
     protected $declineCode;
+    protected $AVSCV2;
+    protected $vpsTxId;
+    protected $addressResult;
+    protected $postCodeResult;
+    protected $cv2Result;
+    protected $giftAid;
+    protected $_3dSecureStatus;
+    protected $cardType;
+    protected $last4Digits;
 
     /**
      * @param array               $options
@@ -44,22 +53,35 @@ class SagepayResponse
         }
         $array = SagepayUtil::decrypt($_GET['crypt'], $this->api->getFormEncryptionPassword());
 
-        $this->setStatus($array['Status'])
-             ->setStatusDetail($array['StatusDetail'])
-             ->setVendorTxCode($array['VendorTxCode'])
-             ->setAmount($array['Amount'])
-             ->setTxAuthNo($array['TxAuthNo'])
-             ->setDeclineCode($array['DeclineCode']);
+        $this
+            ->setVendorTxCode($array['VendorTxCode'])
+            ->setVpsTxId(isset($array['VPSTxId']) ? $array['VPSTxId'] : null)
+            ->setStatus($array['Status'])
+            ->setStatusDetail($array['StatusDetail'])
+            ->setAVSCV2(isset($array['AVSCV2']) ? $array['AVSCV2'] : null)
+            ->setAddressResult(isset($array['AddressResult']) ? $array['AddressResult'] : null)
+            ->setPostCodeResult(isset($array['PostCodeResult']) ? $array['PostCodeResult'] : null)
+            ->setCv2Result(isset($array['CV2Result']) ? $array['CV2Result'] : null)
+            ->setGiftAid(isset($array['GiftAid']) ? $array['GiftAid'] : null)
+            ->set_3dSecureStatus(isset($array['3DSecureStatus']) ? $array['3DSecureStatus'] : null)
+            ->setCardType(isset($array['CardType']) ? $array['CardType'] : null)
+            ->setLast4Digits(isset($array['Last4Digits']) ? $array['Last4Digits'] : null)
+            ->setLast4Digits(isset($array['Last4Digits']) ? $array['Last4Digits'] : null)
+            ->setAmount($array['Amount'])
+            ->setTxAuthNo(isset($array['TxAuthNo']) ? $array['TxAuthNo'] : null)
+            ->setDeclineCode(isset($array['DeclineCode']) ? $array['DeclineCode'] : null)
+        ;
 
         return $this;
     }
 
-    public function isComplete()
+    public function getArrayForDetails()
     {
-        if($this->status == SagepayTransactionType::COMPLETE)
-            return true;
-        return false;
+        $all = get_object_vars($this);
+        unset($all['api']);
+        return $all;
     }
+
     
     /**
      * Get the value of declineCode
@@ -177,6 +199,186 @@ class SagepayResponse
     public function setAmount($amount)
     {
         $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of vpsTxId
+     */ 
+    public function getVpsTxId()
+    {
+        return $this->vpsTxId;
+    }
+
+    /**
+     * Set the value of vpsTxId
+     *
+     * @return  self
+     */ 
+    public function setVpsTxId($vpsTxId)
+    {
+        $this->vpsTxId = $vpsTxId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of addressResult
+     */ 
+    public function getAddressResult()
+    {
+        return $this->addressResult;
+    }
+
+    /**
+     * Set the value of addressResult
+     *
+     * @return  self
+     */ 
+    public function setAddressResult($addressResult)
+    {
+        $this->addressResult = $addressResult;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of postCodeResult
+     */ 
+    public function getPostCodeResult()
+    {
+        return $this->postCodeResult;
+    }
+
+    /**
+     * Set the value of postCodeResult
+     *
+     * @return  self
+     */ 
+    public function setPostCodeResult($postCodeResult)
+    {
+        $this->postCodeResult = $postCodeResult;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cv2Result
+     */ 
+    public function getCv2Result()
+    {
+        return $this->cv2Result;
+    }
+
+    /**
+     * Set the value of cv2Result
+     *
+     * @return  self
+     */ 
+    public function setCv2Result($cv2Result)
+    {
+        $this->cv2Result = $cv2Result;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of giftAid
+     */ 
+    public function getGiftAid()
+    {
+        return $this->giftAid;
+    }
+
+    /**
+     * Set the value of giftAid
+     *
+     * @return  self
+     */ 
+    public function setGiftAid($giftAid)
+    {
+        $this->giftAid = $giftAid;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of _3dSecureStatus
+     */ 
+    public function get_3dSecureStatus()
+    {
+        return $this->_3dSecureStatus;
+    }
+
+    /**
+     * Set the value of _3dSecureStatus
+     *
+     * @return  self
+     */ 
+    public function set_3dSecureStatus($_3dSecureStatus)
+    {
+        $this->_3dSecureStatus = $_3dSecureStatus;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cardType
+     */ 
+    public function getCardType()
+    {
+        return $this->cardType;
+    }
+
+    /**
+     * Set the value of cardType
+     *
+     * @return  self
+     */ 
+    public function setCardType($cardType)
+    {
+        $this->cardType = $cardType;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of last4Digits
+     */ 
+    public function getLast4Digits()
+    {
+        return $this->last4Digits;
+    }
+
+    /**
+     * Set the value of last4Digits
+     *
+     * @return  self
+     */ 
+    public function setLast4Digits($last4Digits)
+    {
+        $this->last4Digits = $last4Digits;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of AVSCV2
+     */ 
+    public function getAVSCV2()
+    {
+        return $this->AVSCV2;
+    }
+
+    /**
+     * Set the value of AVSCV2
+     *
+     * @return  self
+     */ 
+    public function setAVSCV2($AVSCV2)
+    {
+        $this->AVSCV2 = $AVSCV2;
 
         return $this;
     }
